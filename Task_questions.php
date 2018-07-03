9853fc43eb9b7f090b726b8b9bc9b1528c6bc136
achursin <37906195+achursin@users.noreply.github.com>
2018-07-03 10:45:51 +0300

    Update Task_questions.php

--- Task_questions.php
+++ Task_questions.php
@@ -2,94 +2,56 @@
 class Question
 {
     public $text;                // Текст вопроса
-    public $points = 5;          // Число баллов, по умолчанию 5
+    public $points;              // Число баллов
     public $answers;             // Варианты ответов
     public $correctAnswer;       // Правильный ответ
-    public $correctAnswerWord;   // Правильный ответ словами
-}
 
-// Функция, создающая массив с вопросами:
-function createQuestions(){
-    $questions = []; // Создаём пустой массив
+    public function __construct($text, $answers, $correctAnswer)
+    {
+        $this->text = $text;
+        $this->answers = $answers;
+        $this->correctAnswer = $correctAnswer;
+    }
 
-    $q = new Question;
-    $q->text="В какой команде играет Месси?"; // Вопрос
-    $q->points=5; // Баллы за ответ
-    $q->answers= array('a' => 'Барселона', 'b' => 'Бавария', 'c' => 'Манчестер Сити', 'd' => 'ПСЖ'); // Варианты ответа
-    $q->correctAnswer='a'; // Правильный вариант ответа
-    $q->correctAnswerWord='Барселона'; // Правильный вариант ответа словами
-    $questions[] = $q; // Кладём вопрос в массив
+    function createQuestions(){
+    $questions = [];
 
-    $q = new Question;
-    $q->text="Сколько золотых мячей у Месси?"; // Вопрос
-    $q->points=10; // Баллы за ответ
-    $q->answers= array('a' => 'один', 'b' => 'три', 'с'=>'пять', 'd'=>'шесть'); // Варианты ответа
-    $q->correctAnswer='c'; // Правильный вариант ответа
-    $q->correctAnswerWord='пять'; // Правильный вариант ответа словами
-    $questions[] = $q; // Кладём вопрос в массив
+    $myobj = new Question;
+    $myobj->text="В какой команде играет Месси?";
+    $myobj->points=5;
+    $myobj->answers= array('a' => 'Барселона', 'b' => 'Бавария', 'c' => 'Манчестер Сити', 'd' => 'ПСЖ');
+    $myobj->correctAnswer='a';
+    $questions[] = $myobj;
 
-    $q = new Question;
-    $q->text="В финале какого турнира не играл Месси?"; // Вопрос
-    $q->points=15; // Баллы за ответ
-    $q->answers=array('a'=>'в финале ЛЧ', 'b' => 'в финале КОНМЕБОЛ', 'c'=>'в финале кубка Испании','d'=>'в финале ЧЕ'); // Варианты ответа
-    $q->correctAnswer='d'; // Правильный вариант ответа
-    $q->correctAnswerWord='в финале ЧЕ'; // Правильный вариант ответа словами
-    $questions[] = $q; // Кладём вопрос в массив
+    $myobj = new Question;
+    $myobj->text="Сколько золотых мячей у Месси?";
+    $myobj->points=10;
+    $myobj->answers= array('a' => 'один', 'b' => 'три', 'с'=>'пять', 'd'=>'шесть');
+    $myobj->correctAnswer='c';
+    $questions[] = $myobj;
+
+    $myobj = new Question;
+    $myobj->text="В финале какого турнира не играл Месси?";
+    $myobj->points=15;
+    $myobj->answers=array('a'=>'в финале ЛЧ', 'b' => 'в финале КОНМЕБОЛ', 'c'=>'в финале кубка Испании','d'=>'в финале ЧЕ');
+    $myobj->correctAnswer='d';
+    $questions[] = $myobj;
 
     return $questions; // Возвращаем массив
-
-}
-// Функция выводящая список вопросов с вариантами ответов
-function printQuestions($questions)
-{
-    $number = 1; // Номер вопроса
-    foreach ($questions as $question) {
-        echo "{$number}. {$question->text}\n\n<br>"; // Выводим номер вопроса и сам вопрос
-        echo "Варианты ответов:\n<br>";
-        foreach ($question->answers as $letter => $answer) {
-            echo "  {$letter}. {$answer}\n<br>"; // Выводим варианты ответов
-        }
-        $number++; // Добавляем вопрос
     }
-}
 
-// Функция выводящая правильные ответы и набранные баллы
-function checkAnswers($questions, $answers, $answersWord){
-    // Проверяем, что число ответов равно числу вопросов (защищаемся от ошибки)
-    if (count($questions) != count($answers)) {
-        die("Число ответов и вопросов не совпадает\n<br>");
-    }
-    $pointsTotal = 0; // Сколько набрано баллов
-    $pointsMax = 0; // Сколько можно набрать баллов при всех правильных ответах
-    $correctAnswer = 0; // Сколько ответов верно
-    $totalQuestions = count($questions); // Сколько всего вопросов
-    for ($i = 0; $i<count($questions); $i++){
-        $answerWord = $answersWord[$i]; // Текущий ответ словом
-        $question = $questions[$i]; // Текущий вопрос
-        $answer = $answers[$i]; // Текущий ответ
-        $pointsMax += $question->points; // Считаем максимальную сумму баллов
-        // Проверяем правильность ответов
-        if ($answer == $question->correctAnswer) {
-            $correctAnswer++; // Добавляем правильные ответы
-            $pointsTotal += $question->points; // Баллов набрано
-        } else {
-            $number = $i + 1; // Добавляем неправильный ответ
-            echo "Неправильный ответ на вопрос №{$number} ({$question->text})\n<br> Хочешь узнать ответ? <span title='{$answer}. {$answerWord}'>Наведи на этот текст</span>\n<br>";
+    function printQuestions($questions){
+    $number = 1;
+    foreach ($questions as $key => $question) {
+        echo nl2br("{$number}. {$question->text}\n");
+        echo nl2br("Варианты ответов:\n");
+                foreach ($this->answers as $letter => $answer) {
+            echo nl2br("{$letter}. {$answer}\n");
+            }
+        $number++;
         }
     }
-    echo "Правильных ответов: {$correctAnswer} из {$totalQuestions}, баллов набрано: {$pointsTotal} из {$pointsMax}\n"; // Выводим итог
 }
 
 $questions = createQuestions();
-printQuestions($questions); // Выводим список вопросов с вариантами ответов
-$answers = array('a', 'b', 'd'); // Ответы
-$answersWord = array('Барселона', 'пять', 'в финале ЧЕ'); // Ответы словами
-checkAnswers($questions, $answers, $answersWord); // Выводим правильные ответы и набранные баллы
-
-?>
-
-<style>
-    span{
-        cursor: pointer;
-    }
-</style>
+printQuestions($questions);
