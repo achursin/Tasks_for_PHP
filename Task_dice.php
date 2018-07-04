c8221e3b60ae3329d7f21bead9eb0f87b7adcf38
achursin <37906195+achursin@users.noreply.github.com>
2018-07-04 16:51:26 +0300

    Create Task_dice.php

---
+++ Task_dice.php
@@ -0,0 +1,22 @@
+<?php
+// Игра с кубиками через условие
+error_reporting(-1);
+$FirstCastPlayer = mt_rand(1,6);
+$SecondCastPlayer = mt_rand(1,6);
+$SumCastPlayer = $FirstCastPlayer + $SecondCastPlayer;
+echo nl2br("У игрока выпало: " . $SumCastPlayer .PHP_EOL);
+$FirstCastComputer = mt_rand(1,6);
+$SecondCastComputer = mt_rand(1,6);
+$SumCastComputer = $FirstCastComputer + $SecondCastComputer;
+echo nl2br("У компьютера выпало: " . $SumCastComputer .PHP_EOL);
+// Проверяем кто победит
+if ($SumCastPlayer == $SumCastComputer) {
+	echo "У компьютера и игрока выпало одинковое количество, ничья";
+}
+elseif ($SumCastPlayer>$SumCastComputer) {
+	echo "Победил игрок";
+}
+else {
+	echo "Победил компьютер";
+}
+?>
