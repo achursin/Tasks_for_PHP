e67dc9c629a8cec28386041f393b892e947c16e4
achursin <37906195+achursin@users.noreply.github.com>
2018-07-05 17:34:14 +0300

    Update Task_palindrome.php

--- Task_palindrome.php
+++ Task_palindrome.php
@@ -5,7 +5,7 @@ $str = mb_strtolower ($str = str_replace(" ", "", $str)) . ' ' ;
 $letters1 = preg_split('//u', $str, null, PREG_SPLIT_NO_EMPTY);
 unset($letters1[count($letters1) - 1]);
 $letters2 = array_values(array_reverse($letters1, true));
-echo nl2br(PHP_EOL. implode (' ', $letters2) . ' ');
+echo PHP_EOL. implode (' ', $letters2) . ' ';
 if ($letters1 === $letters2)
     {echo " это - Палиндром";}
     else echo " это - не палиндром";
